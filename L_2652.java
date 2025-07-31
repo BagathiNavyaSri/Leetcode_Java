6321889ae82796bd7081f5760801d70ad14451e4
Bagathi Navya sri <bagathinavyasri3011@gmail.com>
2025-07-31 20:04:49 +0530

    Leetcode 2652-Sum Multiples

---
+++ L_2652.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class L_2652 {
+    public static int sumOfMultiples(int n) {
+        int sum = 0;
+        for(int i = 1;i<=n;i++){
+            if(i% 3 == 0 || i%5 == 0 || i%7 == 0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sumOfMultiples(n));
+        sc.close();;
+    }
+}
