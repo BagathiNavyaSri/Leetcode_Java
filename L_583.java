b0ceb60e0757fa970f9914d1038ad96d9e238e37
Bagathi Navya sri <bagathinavyasri3011@gmail.com>
2025-09-15 11:20:56 +0530

    Leetcode 583-Delete Operation for Two Strings

---
+++ L_583.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word2.length();
+        int n = word1.length();
+
+        int[][] dp = new int[m+1][n+1];
+
+        for(int j = 0; j <= n; j++){     // Filling the values of row 0
+            dp[0][j] = j;
+        }
+        for(int i = 0; i <= m; i++){  //Filling the values of column 0
+            dp[i][0] = i;
+        }
+
+        for(int i = 1; i <= m; i++){      //take <= m and <= n because dp size is m+1,n+1
+            for(int j = 1; j <= n; j++){
+                if(word1.charAt(j-1) == word2.charAt(i-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    int top = dp[i-1][j];
+                    int left = dp[i][j-1];
+                    dp[i][j] = Math.min(top, left)+1;
+                }                
+            }
+        }
+        return dp[m][n];
+    }
+}
