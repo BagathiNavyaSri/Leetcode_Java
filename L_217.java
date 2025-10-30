163c0505a03fdcc1c8bb82695103ec9773b6354e
Bagathi Navya sri <bagathinavyasri3011@gmail.com>
2025-10-30 16:08:00 +0530

    Leetcode 217-Contains Duplicate

---
+++ L_217.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s = new HashSet<>();
+        for(int num : nums){
+            if(s.contains(num)){
+                return true;
+            }
+            s.add(num);
+        }
+        return false;
+    }
+}
+
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Set<Integer> s = new HashSet<>(nums);
+//         return nums.length != s.size();
+//     }
+// }
