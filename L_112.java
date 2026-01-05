d8531f331ba4ebbfb885674b1495911b6aabb966
Bagathi Navya sri <bagathinavyasri3011@gmail.com>
2026-01-05 11:38:47 +0530

    Leetcode 112-Path Sum

---
+++ L_112.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        Queue<TreeNode> path = new LinkedList<>();
+        Queue<Integer> sum = new LinkedList<>();
+        path.add(root);
+        sum.add(root.val);
+        while(!path.isEmpty()){
+            TreeNode node = path.poll();
+            int nodesum = sum.poll();
+
+            if(nodesum == targetSum && node.left == null && node.right == null){
+                return true;
+            }
+            if(node.left != null){
+                path.add(node.left);
+                sum.add(nodesum + node.left.val);
+            }
+            if(node.right != null){
+                path.add(node.right);
+                sum.add(nodesum + node.right.val);
+            }
+        }
+        return false;
+    }
+}
