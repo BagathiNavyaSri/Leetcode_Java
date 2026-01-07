6bd371c27a5fca1eba74342a90d03ab179d0f339
Bagathi Navya sri <bagathinavyasri3011@gmail.com>
2026-01-07 12:15:47 +0530

    Leetcode 222-Count Complete Tree Nodes

--- L_222.java
+++ L_222.java
@@ -15,20 +15,53 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        int count = 0; 
-        if(root == null) return count;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while(!queue.isEmpty()){
-            TreeNode node = queue.poll();
-            count++;
-            if(node.left != null){
-                queue.add(node.left);
-            }
-            if(node.right != null){
-                queue.add(node.right);
-            }
+        int leftdep = leftdepth(root);
+        int rightdep = rightdepth(root);
+        if(leftdep == rightdep){
+            return (int)Math.pow(2, leftdep) - 1;
         }
-        return count;
+        else{
+            return 1 + countNodes(root.left) + countNodes(root.right);
+        }
+    }
+    public int leftdepth(TreeNode root){
+        int dep = 0;
+        while(root != null){
+            dep++;
+            root = root.left;
+        }
+        return dep;
+    }
+    public int rightdepth(TreeNode root){
+        int dep = 0;
+        while(root != null){
+            dep++;
+            root = root.right;
+        }
+        return dep;
     }
 }
+
+
+
+//The below one takes time complexity of O(n)
+
+// class Solution {
+//     public int countNodes(TreeNode root) {
+//         int count = 0; 
+//         if(root == null) return count;
+//         Queue<TreeNode> queue = new LinkedList<>();
+//         queue.add(root);
+//         while(!queue.isEmpty()){
+//             TreeNode node = queue.poll();
+//             count++;
+//             if(node.left != null){
+//                 queue.add(node.left);
+//             }
+//             if(node.right != null){
+//                 queue.add(node.right);
+//             }
+//         }
+//         return count;
+//     }
+// }
