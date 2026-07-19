4249eaab82ce8780a03398d8d8b6e9a2d7c7c991
Bagathi Navya sri <bagathinavyasri3011@gmail.com>
2026-07-19 20:34:38 +0530

    Leetcode 1004-Max Consecutive Ones III

--- L_1004.java
+++ L_1004.java
@@ -1,25 +1,47 @@
+//MORE OPTIMIZED
 class Solution {
-    public int longestOnes(int[] nums, int k) {
-        int left = 0; 
-        int currLen = 0;
-        int maxLen = 0;
-        int z_count = 0;
+public int longestOnes(int[] nums, int k) {
+        int n = nums.length;
+        int left = 0, z_count = 0;
 
-        for(int right = 0; right < nums.length; right++){
+        for(int right = 0; right < n; right++){
             if(nums[right] == 0){
                 z_count++;
             }
 
-            while(z_count > k){
+            if(z_count > k){
                 if(nums[left] == 0){
                     z_count--;
                 }
                 left++;
             }
-
-            currLen = right - left + 1;
-            maxLen = Math.max(maxLen, currLen);
-        } 
-        return maxLen;
+        }
+        return n-left;
     }
 }
+
+
+// class Solution {
+// public int longestOnes(int[] nums, int k) {
+//         int n = nums.length;
+//         int left = 0, z_count = 0;
+//         int curr_len = 0, max_len = 0;
+
+//         for(int right = 0; right < n; right++){
+//             if(nums[right] == 0){
+//                 z_count++;
+//             }
+            
+//             while(z_count > k){
+//                 if(nums[left] == 0){
+//                     z_count--;
+//                 }
+//                 left++;
+//             }
+//             curr_len = right-left+1;
+//             max_len = Math.max(curr_len, max_len);
+//         }
+//         return max_len;
+//     }
+// }
+
