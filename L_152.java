b0334722dd7727742cfce5da2ae60ec3600f2554
Bagathi Navya sri <bagathinavyasri3011@gmail.com>
2026-07-26 18:50:40 +0530

    Leetcode 152-Maximum Product Subarray

---
+++ L_152.java
@@ -0,0 +1,20 @@
+class Solution {
+    // Here we come from either sides to optimize it instead of using two nested-for loops
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int maxprod = Integer.MIN_VALUE;
+        int prefix = 1;
+        int suffix = 1;
+
+        for(int i = 0; i < n; i++){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+
+            maxprod = Math.max(maxprod, Math.max(prefix, suffix));
+        } 
+        return maxprod;
+    }
+}
